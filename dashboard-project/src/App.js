d75b83dae97d12247c7d2d0a72b751a1bbd76c06
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-21 11:06:50 -0600

    Dashboard, Pages, Apps and Charts Routes Done

--- dashboard-project/src/App.js
+++ dashboard-project/src/App.js
@@ -43,8 +43,31 @@ const App = () => {
 
           <div>
             <Routes>
+              {/* Dashboard */}
               <Route path='/' element='ECommerce'/>
               <Route path='/ecommerce' element='ECommerce'/>
+
+              {/* Pages */}
+              <Route path='/orders' element='Orders'/>
+              <Route path='/employees' element='Employees'/>
+              <Route path='/customers' element='Customers'/>
+
+              {/* Apps */}
+              <Route path='/kanban' element='Kanban'/>
+              <Route path='/editor' element='Editor'/>
+              <Route path='/calendar' element='Calendar'/>
+              <Route path='/color-picker' element='ColorPicker'/>
+
+              {/* Charts */}
+              <Route path='/line' element='Line'/>
+              <Route path='/area' element='Area'/>
+              <Route path='/bar' element='Bar'/>
+              <Route path='/pie' element='Pie'/>
+              <Route path='/financial' element='Financial'/>
+              <Route path='/color-mapping' element='ColorMapping'/>
+              <Route path='/pyramid' element='Pyramid'/>
+              <Route path='/stacked' element='Stacked'/>
+
             </Routes>
           </div>
 
