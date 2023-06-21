fe3082129a71f55b4b842693daa4d692c60937fb
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-21 10:56:10 -0600

    App.js Sidebar and Navbar Basic Layout Done

--- dashboard-project/src/App.js
+++ dashboard-project/src/App.js
@@ -5,6 +5,9 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const App = () => {
+
+  const activeMenu = false;
+
   return (
     <div>
       <BrowserRouter>
@@ -14,16 +17,35 @@ const App = () => {
           <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
             <TooltipComponent content='Settings' position='Top'>
               <button type='button' 
-                      className='text-3xl 
-                                 p-3 
-                                 hover:drop-shadow-xl 
-                                 hover:bg-light-gray
-                                 text-white'
+                      className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
                       style={{ background: 'blue', borderRadius: '50%' }}
               >
                 <FiSettings />
               </button>
             </TooltipComponent>
+          </div>
+
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              Sidebar
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              Sidebar
+            </div>
+          )}
+
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className='fixed mad:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              NavBar
+            </div>            
+          </div>
+
+          <div>
+            <Routes>
+              <Route path='/' element='ECommerce'/>
+              <Route path='/ecommerce' element='ECommerce'/>
+            </Routes>
           </div>
 
         </div>
