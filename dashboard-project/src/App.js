0e825d05437dca08dbf95731810147954b69b3ea
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-21 15:26:40 -0600

    ContextApi and SideBar closing button Done

--- dashboard-project/src/App.js
+++ dashboard-project/src/App.js
@@ -4,11 +4,12 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { NavBar, Footer, SideBar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
 
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
 
   return (
     <div>
--- dashboard-project/src/index.js
+++ dashboard-project/src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from './App';
 import './index.css';
+import { ContextProvider } from "./contexts/ContextProvider";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
+    document.getElementById('root')
+    );
