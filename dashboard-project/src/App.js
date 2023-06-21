17e629a064dfefe84e87418f4ecf814b272f7296
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-21 13:47:29 -0600

    Components and Pages imports Bug Fixed

--- dashboard-project/src/App.js
+++ dashboard-project/src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { NavBar, Footer, SideBar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
 const App = () => {
--- dashboard-project/src/components/index.jsx
+++ dashboard-project/src/components/index.jsx
@@ -1,7 +1,7 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
-export { default as Sidebar } from './Sidebar';
-export { default as Navbar } from './Navbar';
+export { default as SideBar } from './SideBar';
+export { default as NavBar } from './NavBar';
 export { default as Footer } from './Footer';
 export { default as Cart } from './Cart';
 export { default as Chat } from './Chat';
