1240659ec963f63d192603abe2618ea8c587b983
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-22 17:53:24 -0600

    Ecommerce page Stacked.jsx Chart Done

--- dashboard-project/src/pages/Ecommerce.jsx
+++ dashboard-project/src/pages/Ecommerce.jsx
@@ -106,7 +106,21 @@ const Ecommerce = () => {
                     />
                   </div>
 
+                  <div className='mt-10'>
+                    <Button
+                      color='white'
+                      bgColor='blue'
+                      text='Download Report'
+                      borderRadius='10px'
+                    />
+                  </div>
+
                 </div>
+
+                <div>
+                  <Stacked width='320px' height='360'/>
+                </div>
+
               </div>
             </div>
       </div>
