87b682a6b01d6b8c392e07b98e734ba1231fc96f
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-23 01:43:03 -0600

    Cart.jsx Complete component Done and Button.jsx Refactor

--- dashboard-project/src/components/Button.jsx
+++ dashboard-project/src/components/Button.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const Button = ({ bgColor, color, size, text, borderRadius }) => {
-  return (
-    <button 
-      type='button' 
-      style={{ backgroundColor: bgColor, color, borderRadius }} 
-      className={`text-${size} p-3 hover:drop-shadow-xl`}
-    >
-      {text}
-    </button>
-  )
-}
+import { useStateContext } from '../contexts/ContextProvider';
 
-export default Button
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+  const { setIsClicked, initialState } = useStateContext();
+
+  return (
+    <button
+      type="button"
+      onClick={() => setIsClicked(initialState)}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+    >
+      {icon} {text}
+    </button>
+  );
+};
+
+export default Button;
